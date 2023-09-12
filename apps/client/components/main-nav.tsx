27ed658356c9520454a80228c83346447dc3b90b
av-erencelik <erenmemo05@gmail.com>
2023-09-12 12:45:06 +0300

    changed navigations to not have dropdown menu

--- apps/client/components/main-nav.tsx
+++ apps/client/components/main-nav.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { siteConfig } from '../config/site';
-import { NavItem, ProductNavItem } from '../types';
+import { NavItem } from '../types';
 import { cn, navigationMenuTriggerStyle } from '@e-commerce-monorepo/ui';
 import { usePathname } from 'next/navigation';
-import NavigationDropdown from './navigation-dropdown';
+
 type MainNavProps = {
-  navItems: Array<NavItem | ProductNavItem>;
+  navItems: Array<NavItem>;
   isAdmin: boolean;
 };
 
@@ -31,9 +31,6 @@ const MainNav = ({ navItems, isAdmin }: MainNavProps) => {
       {navItems.length > 0 && (
         <nav className="hidden gap-2 md:flex">
           {navItems.map((navItem, index) => {
-            if ('isCategory' in navItem) {
-              return <NavigationDropdown key={index} item={navItem} />;
-            }
             return (
               <Link
                 href={navItem.href}
