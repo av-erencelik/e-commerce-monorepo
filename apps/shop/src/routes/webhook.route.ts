72d3409e38fa7e1b5d2347f596b049c525bacfda
av-erencelik <erenmemo05@gmail.com>
2023-10-21 15:33:01 +0300

    express raw on webhook

--- apps/shop/src/routes/webhook.route.ts
+++ apps/shop/src/routes/webhook.route.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import express from 'express';
 import webhookController from '../controllers/webhook.controller';
 
 const webhookRouter = Router();
 
-webhookRouter.post('/shop/webhook', webhookController.handleEvent);
+webhookRouter.post(
+  '/shop/webhook',
+  express.raw({ type: '*/*' }),
+  webhookController.handleEvent
+);
 
 export default webhookRouter;
