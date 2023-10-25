c408b9604e71b9ce8261c9e61f65fcc6c6e97067
av-erencelik <erenmemo05@gmail.com>
2023-10-25 13:44:41 +0300

    added custom 404 page

---
+++ apps/client/app/not-found.tsx
@@ -0,0 +1,21 @@
+import { Shell, buttonVariants } from '@e-commerce-monorepo/ui';
+import Link from 'next/link';
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <Shell className="text-center mx-auto my-auto h-[500px]">
+      <section className="space-y-4">
+        <h1 className="text-5xl font-bold">404</h1>
+        <p className="text-xl tracking-tight">
+          We can&apos;t find the page you&apos;re looking for.
+        </p>
+        <Link href="/" className={buttonVariants()}>
+          Continue Shopping
+        </Link>
+      </section>
+    </Shell>
+  );
+};
+
+export default NotFoundPage;
